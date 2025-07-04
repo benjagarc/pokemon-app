96e69789002cdee3b46032106c7a5247b5c1b71a
bgarciabtk <bgarcia@btkfarma.com>
2025-07-03 20:14:54 -0600

    add pokemon common interfaces

---
+++ interfaces/pokemon.ts
@@ -0,0 +1,14 @@
+export interface PokemonInterface {
+  description: string;
+  genus: string;
+  id: number;
+  image: string;
+  name: string;
+  type: string;
+}
+
+export interface LenguageInterface {
+  language: {
+    name: string;
+  };
+}
