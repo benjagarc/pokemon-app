8970c7e225bf7999de174e7a99ae1dc0b6a4f648
bgarciabtk <bgarcia@btkfarma.com>
2025-07-03 20:14:03 -0600

    udpate request to get Pokemon

--- api/pokemon.ts
+++ api/pokemon.ts
@@ -1,6 +1,38 @@
+import { LenguageInterface } from "@/interfaces/pokemon";
 import { api } from "@lib/axios";
 
 export const getPokemonList = async () => {
-  const { data } = await api.get("/pokemon?limit=10&offset=0");
-  return data;
+  const { data } = await api.get("/pokemon?limit=5");
+  const pokemons = await Promise.all(
+    data.results.map(async (poke: { name: string }) => {
+      const [pokemonRes, speciesRes] = await Promise.all([
+        api.get(`/pokemon/${poke.name}`),
+        api.get(`/pokemon-species/${poke.name}`),
+      ]);
+
+      const pokemon = pokemonRes.data;
+      const species = speciesRes.data;
+
+      const description =
+        species.flavor_text_entries
+          .find((entry: LenguageInterface) => entry.language.name === "en")
+          ?.flavor_text?.replace(/\f|\n/g, " ") ?? "No description available.";
+
+      const genus =
+        species.genera.find(
+          (entry: LenguageInterface) => entry.language.name === "en"
+        )?.genus ?? "Pokémon";
+
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other["official-artwork"].front_default,
+        type: pokemon.types[0].type.name,
+        genus,
+        description,
+      };
+    })
+  );
+
+  return pokemons;
 };
