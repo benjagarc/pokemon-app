0ad6a8ea4e155656f926bb85b8b80f37e3c1e780
bgarciabtk <bgarcia@btkfarma.com>
2025-07-05 17:58:46 -0600

    update query get games essentials

--- hooks/useGamesEssentials.ts
+++ hooks/useGamesEssentials.ts
@@ -1,10 +1,10 @@
 import { getGamesEssentials } from "@/api/pokemon";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGamesEssentials = () => {
+export const useGamesEssentials = (limit: number) => {
   return useQuery({
-    queryKey: ["games-essentials"],
-    queryFn: () => getGamesEssentials(),
+    queryKey: ["games-essentials", limit],
+    queryFn: () => getGamesEssentials(limit),
     retry: 2,
     refetchOnWindowFocus: false,
     staleTime: 1000 * 60 * 10,
