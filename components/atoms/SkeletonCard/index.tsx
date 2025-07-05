5eb5a122879d0de97543658e2e4478266a0d1195
bgarciabtk <bgarcia@btkfarma.com>
2025-07-05 17:58:02 -0600

    update background skeleton

--- components/atoms/SkeletonCard/index.tsx
+++ components/atoms/SkeletonCard/index.tsx
@@ -1,6 +1,6 @@
 export const SkeletonCard = () => {
   return (
-    <div className="glass border border-gray-200 rounded-xl p-4 shadow-md text-center relative animate-pulse space-y-3">
+    <div className="bg-pokemon-glass glass border border-gray-200 rounded-xl p-4 shadow-md text-center relative animate-pulse space-y-3">
       <div className="w-32 h-32 mx-auto bg-black/40 rounded-lg" />
       <div className="h-4 w-3/5 mx-auto bg-black/50 rounded" />
       <div className="h-3 w-2/4 mx-auto bg-black/40 rounded" />
