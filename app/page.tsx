4d82ac6f867340c89401bb75803df440a9d789de
bgarciabtk <bgarcia@btkfarma.com>
2025-07-04 19:55:02 -0600

    update query, pass limit and offset

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { getPokemonList } from "@/api/pokemon";
+import { getPaginatedPokemons } from "@/api/pokemon";
 import Home from "@/components/pages/Home";
 export default async function HomePage() {
-  const pokemonList = await getPokemonList();
+  const pokemonList = await getPaginatedPokemons(15,0);
   return (
     <>
       <Home pokemonList={pokemonList}/>
