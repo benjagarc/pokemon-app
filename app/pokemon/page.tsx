613a227fc989237b33150b529f4e3143a6e4909d
bgarciabtk <bgarcia@btkfarma.com>
2025-07-04 21:22:13 -0600

    implement searchBar

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import SecondaryCard from "@/components/atoms/secondaryCard";
+import SearchBar from "@/components/organism/SearchBar";
 import { usePokemonList } from "@/hooks/usePokemonList";
+import { PokemonInterface } from "@/interfaces/pokemon";
+import { useState } from "react";
 
 interface PageProps {
   searchParams: { page?: string };
@@ -12,26 +15,34 @@ export default function PokemonPage({ searchParams }: PageProps) {
   const limit = 12;
   const offset = (page - 1) * limit;
   const { data: pokemons } = usePokemonList(limit, offset);
+  const [pokemonSearched, setPokemonSearched] = useState(
+    {} as PokemonInterface
+  );
   return (
     <>
+      <SearchBar handleResults={(result) => setPokemonSearched(result)} />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-        {pokemons?.map((pokemon) => (
-          <SecondaryCard key={pokemon.id} {...pokemon} />
-        ))}
+        {!pokemonSearched?.id &&
+          pokemons?.map((pokemon) => (
+            <SecondaryCard key={pokemon.id} {...pokemon} />
+          ))}
+        {pokemonSearched?.id && <SecondaryCard {...pokemonSearched} />}
       </div>
-      <div className="flex justify-between text-white">
-        {page > 1 && (
-          <a href={`/pokemon?page=${page - 1}`} className="hover:underline">
-            ← Prev
+      {!pokemonSearched?.id && (
+        <div className="flex justify-between text-white">
+          {page > 1 && (
+            <a href={`/pokemon?page=${page - 1}`} className="hover:underline">
+              ← Prev
+            </a>
+          )}
+          <a
+            href={`/pokemon?page=${page + 1}`}
+            className="hover:underline ml-auto"
+          >
+            Next →
           </a>
-        )}
-        <a
-          href={`/pokemon?page=${page + 1}`}
-          className="hover:underline ml-auto"
-        >
-          Next →
-        </a>
-      </div>
+        </div>
+      )}
     </>
   );
 }
