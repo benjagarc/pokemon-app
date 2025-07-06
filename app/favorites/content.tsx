a1d8653a62a68dab61be53a4d84c70f69cd035e6
bgarciabtk <bgarcia@btkfarma.com>
2025-07-05 21:37:06 -0600

    add loading statement to favorites

--- app/favorites/content.tsx
+++ app/favorites/content.tsx
@@ -3,10 +3,16 @@
 import SecondaryCard from "@/components/atoms/secondaryCard";
 import SkeletonCard from "@/components/atoms/SkeletonCard";
 import { useFavoritesStore } from "@/stores/favorite";
-import { Suspense } from "react";
+import { useLayoutEffect, useState } from "react";
 
 export const ContentFavorite = () => {
   const favorites = useFavoritesStore((state) => state.favorites);
+
+  const [loading, setLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    setLoading((prev) => !prev);
+  }, []);
 
   return (
     <section className="max-w-6xl mx-auto px-4 py-8">
@@ -14,19 +20,33 @@ export const ContentFavorite = () => {
         Your Favorite Pokémon
       </h1>
 
-      <Suspense fallback={<SkeletonCard />}>
-        {favorites.length === 0 ? (
-          <p className="text-pokemon-red">
-            You haven&apos;t added any Pokémon to favorites yet
-          </p>
-        ) : (
+      {loading && (
+        <>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+            {Array(6)
+              .fill(0)
+              .map((_, index) => (
+                <SkeletonCard key={index} />
+              ))}
+          </div>
+        </>
+      )}
+
+      {!loading && favorites?.length === 0 && (
+        <p className="text-pokemon-red">
+          You haven&apos;t added any Pokémon to favorites yet
+        </p>
+      )}
+
+      {!loading && favorites?.length > 0 && (
+        <>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
             {favorites.map((pokemon) => (
               <SecondaryCard genus={""} key={pokemon.id} {...pokemon} />
             ))}
           </div>
-        )}
-      </Suspense>
+        </>
+      )}
     </section>
   );
 };
