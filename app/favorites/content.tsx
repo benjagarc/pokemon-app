a58dbe5d390d6d7a2c238f45d8291fa70ecb29e8
bgarciabtk <bgarcia@btkfarma.com>
2025-07-05 21:28:08 -0600

    add suspense cards

--- app/favorites/content.tsx
+++ app/favorites/content.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import SecondaryCard from "@/components/atoms/secondaryCard";
+import SkeletonCard from "@/components/atoms/SkeletonCard";
 import { useFavoritesStore } from "@/stores/favorite";
+import { Suspense } from "react";
 
 export const ContentFavorite = () => {
   const favorites = useFavoritesStore((state) => state.favorites);
@@ -12,17 +14,19 @@ export const ContentFavorite = () => {
         Your Favorite Pokémon
       </h1>
 
-      {favorites.length === 0 ? (
-        <p className="text-pokemon-red">
-          You haven&apos;t added any Pokémon to favorites yet
-        </p>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-          {favorites.map((pokemon) => (
-            <SecondaryCard genus={""} key={pokemon.id} {...pokemon} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<SkeletonCard />}>
+        {favorites.length === 0 ? (
+          <p className="text-pokemon-red">
+            You haven&apos;t added any Pokémon to favorites yet
+          </p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+            {favorites.map((pokemon) => (
+              <SecondaryCard genus={""} key={pokemon.id} {...pokemon} />
+            ))}
+          </div>
+        )}
+      </Suspense>
     </section>
   );
 };
