1413e3b531adbbf3337736630ee056f319d89ab4
bgarciabtk <bgarcia@btkfarma.com>
2025-07-05 22:03:19 -0600

    add lazy loading to secundary sections

--- components/pages/Home/index.tsx
+++ components/pages/Home/index.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { HomeProps } from "./interface";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 import Carousel from "@/components/molecules/Carousel/Carousel";
 import { SwiperSlide } from "swiper/react";
 import PokemonCard from "@/components/atoms/PrincipalCard";
-import SecondaryCard from "@/components/atoms/secondaryCard";
 import { useGamesEssentials } from "@/hooks/useGamesEssentials";
-import GameCard from "@/components/atoms/GameCard";
 import { Game } from "@/components/atoms/GameCard/interface";
+import SkeletonCard from "@/components/atoms/SkeletonCard";
+import dynamic from "next/dynamic";
+
+const SecondaryCard = dynamic(() => import("@/components/atoms/secondaryCard"));
+const GameCard = dynamic(() => import("@/components/atoms/GameCard"));
 
 export const Home: FC<HomeProps> = ({ pokemonList }) => {
   const arrayLastEvolution = pokemonList.filter(
@@ -20,6 +23,7 @@ export const Home: FC<HomeProps> = ({ pokemonList }) => {
   );
 
   const { data: gamesEssentials } = useGamesEssentials(3);
+  const fakeCards = Array(3).fill(0);
 
   return (
     <>
@@ -37,17 +41,37 @@ export const Home: FC<HomeProps> = ({ pokemonList }) => {
       </h3>
 
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-6 mt-5 pt-5 place-items-center">
-        {pokemonEssentials.slice(0, 3).map((pokemon) => (
-          <SecondaryCard key={pokemon?.id} {...pokemon} />
-        ))}
+        <Suspense
+          fallback={
+            <>
+              {fakeCards.map((_, index) => (
+                <SkeletonCard key={index} />
+              ))}
+            </>
+          }
+        >
+          {pokemonEssentials.slice(0, 3).map((pokemon) => (
+            <SecondaryCard key={pokemon?.id} {...pokemon} />
+          ))}
+        </Suspense>
       </section>
       <h3 className="text-2xl max-sm:text-2xl text-center font-extrabold capitalize text-black drop-shadow-md break-words my-8">
         Games Essentials
       </h3>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-6 mt-5 pt-5 place-items-center">
-        {gamesEssentials?.map((game: Game) => (
-          <GameCard key={game?.name} game={game} />
-        ))}
+        <Suspense
+          fallback={
+            <>
+              {fakeCards.map((_, index) => (
+                <SkeletonCard key={index} />
+              ))}
+            </>
+          }
+        >
+          {gamesEssentials?.map((game: Game) => (
+            <GameCard key={game?.name} game={game} />
+          ))}
+        </Suspense>
       </section>
     </>
   );
