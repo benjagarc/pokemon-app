7a9bac139ced0a04092a3d9f9564a4d2d7255d7e
bgarciabtk <bgarcia@btkfarma.com>
2025-07-05 22:22:04 -0600

    set margin to navbar fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 import { ReactQueryProvider } from "@/components/Providers/ReactQueryProvider";
+import Navbar from "@/components/molecules/Navbar";
 
 const pixelFont = Press_Start_2P({ subsets: ["latin"], weight: "400" });
 
@@ -20,10 +21,8 @@ export default function RootLayout({
       <body
         className={`${pixelFont.className} relative bg-gradient-to-br from-pokemon-red from-25% via-white/10 via-60% to-pokemon-blue to-80% min-h-screen`}
       >
-        <nav className="w-full px-6 py-4 bg-white/25 backdrop-blur-sm border-b border-white/20 text-white shadow-md">
-          <h1 className="text-xl tracking-wide">Pokédex App</h1>
-        </nav>
-        <main className="max-w-7xl mx-auto p-6 mt-6">
+        <Navbar />
+        <main className="max-w-7xl mx-auto p-6 mt-6 pt-20 max-sm:pt-17">
           <div className="rounded-2xl bg-white/20 backdrop-blur-lg border border-white/20 shadow-lg p-6 min-h-[80vh]">
             <ReactQueryProvider>{children}</ReactQueryProvider>
           </div>
